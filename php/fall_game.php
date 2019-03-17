d9546ffadc95b150cb3c5890af92bea30992b7a3
yasu199 <yasu.hiro09913@gmail.com>
2019-03-17 22:21:30 +0900

    各処理を効率化

--- php/fall_game.php
+++ php/fall_game.php
@@ -2,87 +2,71 @@
     $input_lines = fgets(STDIN);
     $input_lines = explode(' ', $input_lines);
     // 縦要素MAX
+    // ステージの縦の最大値
     $h_max = (int) $input_lines[0];
+    // ステージの横フレームの最大値
     $w_max = (int) $input_lines[1];
+    // ブロック数
     $n_block = (int) $input_lines[2];
-    // 初期状態設置
+    // ステージ全体の初期状態設置
     $all_points = array();
     for ($i = 1;$i <= $h_max; $i++) {
         $inside_points = array();
         for($h = 1; $h <= $w_max; $h++) {
-            $inside_points[] = 0;
+            $inside_points[] = '.';
         }
         $all_points[] = $inside_points;
     }
-    // ブロックの落ちてくる要素情報
+    // 落ちてくるブロックの要素情報
     while ($block_line = trim(fgets(STDIN))) {
         $block_box[] = $block_line;
     }
-    // ブロック要素点検関数
-    // function zero_check($i, $x_block, $left_side, $all_points) {
-    //     $err_check = array();
-    //     $flag = TRUE;
-    //     $line_now = $all_points[$i];
-    //     for ($h = $x_block; $h < $left_side; $h++) {
-    //         if ($line_now[$h] !== 0) {
-    //             $err_check[] = 'err_ブロックがあります';
-    //         }
-    //     }
-    //     if (count($err_check) !== 0) {
-    //         $flag = FALSE;
-    //     }
-    //     return $flag;
-    // }
     foreach ($block_box as $value) {
         $block[] = explode(' ', $value);
     }
+
+    // 落下してくるブロックについて、ステージを.→#へ変更
+    // ブロックひとつづつについて確認
     foreach ($block as $value) {
         // 要素の縦横
+        // ブロックの縦幅
         $h_block = (int) $value[0];
+        // ブロックの横幅
         $w_block = (int) $value[1];
+        // ブロックの左端の位置
         $x_block = (int) $value[2];
-
+        // ブロックの右端の位置
         $left_side = $x_block + $w_block;
-        $flag = TRUE;
+        // ブロックについて、ステージの位置を1行づつ降下
         for ($i = 0; $i < $h_max; $i++) {
-            // 現在行の配列取得
-            if ($flag === TRUE) {
-                $line_now = $all_points[$i];
-                // ================================================
-                $err_check = array();
+            // ブロックの現在行のステージ情報取得
+            $line_now = $all_points[$i];
+            // ブロックの左端から右端まで、ステージ上に障害物があるか確認
+            // あった場合にはブロックの降下処理までをbreak
+            for ($h = $x_block; $h < $left_side; $h++) {
+                if ($line_now[$h] === '#') {
+                    break 2;
+                }
+            }
+            // 障害物がなかった時はブロックがあることをステージに#マークで記載
+            for ($h = $x_block; $h < $left_side; $h++) {
+                $all_points[$i][$h] = '#';
+            }
+            // ブロックが完全にステージに入っていた場合、ブロックの上部分を一段下げるため、
+            // 上部分があった場所について、ステージの#を.へ書き換え
+            if (($i - $h_block) >= 0) {
+              // 抜けた上部分のステージ行をpast_topとして記載
+                $past_top = $i - $h_block;
+                // ブロックが抜けた部分について、ステージの#を.へ書き換え
                 for ($h = $x_block; $h < $left_side; $h++) {
-                    if ($line_now[$h] !== 0) {
-                        $err_check[] = 'err_ブロックがあります';
-                    }
-                }
-                if (count($err_check) === 0) {
-                    for ($h = $x_block; $h < $left_side; $h++) {
-                        $all_points[$i][$h] = 1;
-                    }
-                } else {
-                    $flag = FALSE;
-                }
-                // ===================================================
-
-                // 上部分チェック
-                if ($flag === TRUE) {
-                    if (($i - $h_block) >= 0) {
-                        $past_top = $i - $h_block;
-                        for ($h = $x_block; $h < $left_side; $h++) {
-                            $all_points[$past_top][$h] = 0;
-                        }
-                    }
+                    $all_points[$past_top][$h] = '.';
                 }
             }
         }
     }
     foreach ($all_points as $value) {
         foreach ($value as $value_child) {
-            if ($value_child === 1) {
-                echo '#';
-            } else {
-                echo '.';
-            }
+            echo $value_child;
         }
         echo "\n";
     }
--- php/light.php
+++ php/light.php
@@ -1,19 +1,18 @@
 <?php
     $input_lines = fgets(STDIN);
     $input_lines = explode(' ', $input_lines);
-    // 高さ
+    // 光が入るボックスの高さ
     $h_all = (int) $input_lines[0];
+    // 光が入るボックスの横幅
     $w_all = (int) $input_lines[1];
-    // 様子内
+    // ボックス内部の情報を取得
     while ($data = trim(fgets(STDIN))) {
         $data_line[] = $data;
     }
     foreach ($data_line as $value) {
-        $inline_data[] = str_split($value);
+        $box_data[] = str_split($value);
     }
-    foreach ($inline_data as $value) {
-        $box_data[] = $value;
-    }
+    // ボックス内をブロックごとの行列にして、初期値をすべて0回通過とする
     for ($i = 0; $i < $h_all; $i++) {
         $box_count_inside = array();
         for ($h = 0; $h < $w_all; $h++) {
@@ -21,18 +20,18 @@
         }
         $box_count[] = $box_count_inside;
     }
-    // 方向について関数
+    // 光の来る方向について、行くブロック、光の進行方向を関数で規定
     function to_right($i,$h,$data) {
         if ($data === "\\") {
             $h++;
             $i--;
-            $return = array($i, $h, '下');
+            $return = array($i, $h, 'down');
         } elseif ($data === '/') {
             $h--;
             $i--;
-            $return = array($i, $h, '上');
+            $return = array($i, $h, 'up');
         } else {
-            $return = array($i, $h, '右');
+            $return = array($i, $h, 'right');
         }
         return $return;
     }
@@ -40,83 +39,90 @@
         if ($data === "\\") {
             $h--;
             $i--;
-            $return = array($i, $h, '上');
+            $return = array($i, $h, 'up');
         } elseif ($data === '/') {
             $h++;
             $i--;
-            $return = array($i, $h, '下');
+            $return = array($i, $h, 'down');
         } else {
             $i = $i - 2;
-            $return = array($i, $h, '左');
+            $return = array($i, $h, 'left');
         }
         return $return;
     }
     function to_upside($i, $h, $data) {
         if ($data === "\\") {
             $i = $i - 2;
-            $return = array($i, $h, '左');
+            $return = array($i, $h, 'left');
         } elseif ($data === '/') {
-            $return = array($i, $h, '右');
+            $return = array($i, $h, 'right');
         } else {
             $h--;
             $i--;
-            $return = array($i, $h, '上');
+            $return = array($i, $h, 'up');
         }
         return $return;
     }
     function to_downside($i, $h, $data) {
         if ($data === "\\") {
-            $return = array($i, $h, '右');
+            $return = array($i, $h, 'right');
         } elseif ($data === '/') {
             $i = $i - 2;
-            $return = array($i, $h, '左');
+            $return = array($i, $h, 'left');
         } else {
             $h++;
             $i--;
-            $return = array($i, $h, '下');
+            $return = array($i, $h, 'down');
         }
         return $return;
     }
-    // 初期値設定
-    $flag = '右';
+    // 光の入力について初期値設定
+    $flag = 'right';
     $h = 0;
+    // 光を入力させた後の処理。まずは横方向の動きを、右に進むと仮定してボックス内のブロックを移動させる
+    // 以下において、$i:ボックス内の横方向の位置、$h:ボックス内の縦方向の位置
     for ($i = 0; $i < $w_all; $i++) {
+      // 光の通っているブロックについて、通過回数を+1
         $box_count[$h][$i] = $box_count[$h][$i] + 1;
-        // 右のとき
-        if ($flag === '右') {
+        // 光の進行方向が右のとき
+        if ($flag === 'right') {
             $light_data = to_right($i, $h, $box_data[$h][$i]);
             $i = $light_data[0];
             $h = $light_data[1];
             $flag = $light_data[2];
-        } elseif ($flag === '左') {
-            // 左のとき
+        } elseif ($flag === 'left') {
+            // 光の進行方向が左のとき
             $light_data = to_left($i, $h, $box_data[$h][$i]);
             $i = $light_data[0];
             $h = $light_data[1];
             $flag = $light_data[2];
-        } elseif ($flag === '上') {
-            // 上のとき
+        } elseif ($flag === 'up') {
+            // 光の進行方向が上のとき
             $light_data = to_upside($i, $h, $box_data[$h][$i]);
             $i = $light_data[0];
             $h = $light_data[1];
             $flag = $light_data[2];
         } else {
-            // 下のとき
+            // 光の進行方向が下のとき
             $light_data = to_downside($i, $h, $box_data[$h][$i]);
             $i = $light_data[0];
             $h = $light_data[1];
             $flag = $light_data[2];
         }
+        // 光がボックスの上に抜けた、もしくは下に抜けた場合には処理を終了
         if ($h < 0 || $h >= $h_all) {
             break;
         }
+        // 光がボックスの左に抜けた場合には処理を終了
         if ($i < -1) {
             break;
         }
     }
+    // 光の通った回数について、各ブロックに格納されている通過回数を合算
     $sum = 0;
     foreach ($box_count as $value) {
         $sum = $sum + array_sum($value);
     }
+    // 光の合計通過回数を出力
     echo $sum;
 ?>
